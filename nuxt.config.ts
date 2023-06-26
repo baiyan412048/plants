9caf485df4461c03bb402877556a0bf965d8558e
baiyan412048 <39880236+baiyan412048@users.noreply.github.com>
2023-06-26 19:42:42 +0800

    use @nuxtjs/google-fonts modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,13 @@
 // http://localhost:3000
 // https://api.baiyan777.com
 export default defineNuxtConfig({
-  modules: ['@pinia/nuxt', 'nuxt-icon', '@vueuse/nuxt', '@nuxt/image-edge'],
+  modules: [
+    '@pinia/nuxt',
+    'nuxt-icon',
+    '@vueuse/nuxt',
+    '@nuxt/image-edge',
+    '@nuxtjs/google-fonts'
+  ],
   runtimeConfig: {
     public: {
       apiBaseUrl: 'https://api.baiyan777.com',
@@ -12,21 +18,13 @@ export default defineNuxtConfig({
   css: ['@/assets/base/_reset.sass', '@/assets/base/_base.sass'],
   app: {
     head: {
-      link: [
-        {
-          rel: 'preconnect',
-          href: 'https://fonts.googleapis.com'
-        },
-        {
-          rel: 'preconnect',
-          href: 'https://fonts.gstatic.com'
-        },
-        {
-          rel: 'stylesheet',
-          href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&family=Noto+Serif+JP:wght@400;500;700&display=swap'
-        }
-      ],
       meta: [{ name: 'theme-color', content: '#34BD32' }]
+    }
+  },
+  googleFonts: {
+    families: {
+      'Noto+Sans+JP': [400, 500, 700],
+      'Noto+Serif+JP': [400, 500, 700]
     }
   }
 })
